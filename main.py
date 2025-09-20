3e0fd5974f8a7e01ae4200a30e0f1011175edfb4
Amin <aminullahpuya@gmail.com>
2025-09-20 21:23:07 +0200

    something about lists

--- main.py
+++ main.py
@@ -70,3 +70,26 @@ print(sorted_numbers) # [67, 41, 35, 19, 2, 1]
 print("\n--- index method ---")
 programming_languages = ['Rust', 'Java', 'JavaScript', 'Python', 'C++']
 print(programming_languages.index('Python')) 
+
+print("\n--- Join two lists ---")
+# Join two list:
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
+
